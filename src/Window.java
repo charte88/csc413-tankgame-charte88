a4be02d9b41c7a93cc5476a3fc726cda7dd30b01
Cameron Harte <cameron.j.harte@gmail.com>
2019-10-20 11:11:51 -0700

    Completed a general version of the backend

---
+++ src/Window.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+import java.awt.Dimension;
+
+public class Window {
+    public Window(int width, int height, String title, Game game) {
+        JFrame frame = new JFrame(title);
+
+        // size of frame
+        frame.setPreferredSize(new Dimension(width, height));
+        frame.setMaximumSize(new Dimension(width, height));
+        frame.setMinimumSize(new Dimension(width, height));
+
+        frame.add(game); //add in game class to frame
+        frame.setResizable(false); //cannot resize window
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null); //when game started box will start center of window
+        frame.setVisible(true); //lets us see window
+    }
+}
